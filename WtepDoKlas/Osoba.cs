334312e6ca6fe466f3975db54c5b1131702858b7
Stasszek <staszek906@o2.pl>
2017-11-17 17:42:50 +0100

    Pierwsy Com

---
+++ WtepDoKlas/Osoba.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WtepDoKlas
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+
+        //prop i 2xTab
+
+        public int Wiek { get; private set; } //krutka forma property tylko pobierz
+
+        //modyfikator dostepu
+        public Osoba()
+        {
+            //zawsze się wywołuje przy stworzeniu obiektu klasy
+            Console.WriteLine("Konstruktor domyslny");
+        }
+
+        public Osoba(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public void zmienImie(string noweImie)
+        {
+            imie = noweImie;
+        }
+
+        public string PobierzImie //property do zwracania jakiejs zmiennej
+        {
+            get
+            {
+                return imie;
+            }
+        }
+
+        public string PobierzNazwisko // property set i get
+        {
+            get
+            {
+                return nazwisko;
+            }
+
+            set
+            {
+                if (value.Length > 3)
+                    nazwisko = value;
+            }
+        }
+
+        public void NadajWiek(int wiek)
+        {
+            this.Wiek = wiek;
+        }
+
+        //przeciazenie operatorow porownania
+        public static bool operator ==(Osoba l, Osoba P)
+        {
+            return l.PobierzNazwisko == P.PobierzNazwisko;
+        }
+
+        public static bool operator !=(Osoba l, Osoba P)
+        {
+            return l.PobierzNazwisko != P.PobierzNazwisko;
+        }
+
+        //przeciazenie operatorow dodania
+        public static int operator +(Osoba L, Osoba P)
+        {
+            return L.Wiek + P.Wiek;
+        }
+
+    }
+}
---
+++ WtepDoKlas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WtepDoKlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osb = new Osoba("Janek", "NopeKowalski");
+
+            Console.WriteLine(osb.PobierzImie);
+            osb.zmienImie("Ala");
+            Console.WriteLine(osb.PobierzImie);
+
+
+            Console.WriteLine(osb.PobierzNazwisko);
+            osb.PobierzNazwisko = "Adamski";
+            Console.WriteLine(osb.PobierzNazwisko);
+
+            osb.NadajWiek(22);
+            Console.WriteLine(osb.Wiek);
+
+            Osoba osb2 = osb; // kopiowanie referencji (kopiuje adress w pamieci ram i przypisuje do osb2)
+            osb2.PobierzNazwisko = "Nowe dla 2 osoby";
+            Console.WriteLine(osb.PobierzNazwisko + " " + osb2.PobierzNazwisko);
+
+            Osoba osoba3 = new Osoba("Ola", "Nowe dla 2 osoby");
+            osoba3.NadajWiek(20);
+            bool rowne = osb == osoba3;
+
+            Console.WriteLine(rowne);
+
+            var wynik = osb2 + osoba3;
+            Console.WriteLine(wynik);
+            //osb.Wiek = "asdas";
+
+            Console.Read();
+        }
+    }
+
+
+}
